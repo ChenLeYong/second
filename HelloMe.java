8b223d21341b196f0ea91c5cdf4f6aaddaeed322
Chen Le <chenleyong97@Chens-MacBook-Pro-2.local>
2018-08-04 21:49:52 +0800

    new programme...

---
+++ HelloMe.java
@@ -0,0 +1,7 @@
+public class HelloMe{
+    public static void main(String[]args){
+        System.out.println("Hello Chen Le");
+        System.out.println("Chen Ming is here");
+        System.out.println("HAHAHA");
+    }
+}
